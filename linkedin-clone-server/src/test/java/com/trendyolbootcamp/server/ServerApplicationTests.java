895a68f7a2071d70a1d3eb79affce33b1799f87b
Gökhan Çağrıtekin <gokhan.cagritekin@gmail.com>
2020-12-09 21:56:01 +0300

    Add files via upload

---
+++ linkedin-clone-server/src/test/java/com/trendyolbootcamp/server/ServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.trendyolbootcamp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
